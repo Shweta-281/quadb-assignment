a082d5e4c1e7816923863e351f3cd42671eccb4c
Shweta <shwetasalunkhe281@gmail.com>
2024-01-31 22:52:34 +0530

    first commit

---
+++ quadb-assignment-main/src/Components/Navbar.js
@@ -0,0 +1,11 @@
+import "../Stylesheets/Navbar.css"
+
+const Navbar = ({handleChange}) => {
+    return (
+            <nav className="nav-main">
+                <h2 className="nav-header">Ticket Booking <span className="nav-h">App</span></h2>
+            </nav>
+    )
+}
+
+export default Navbar
